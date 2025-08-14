ac739fbbee38d3039da44bab039ee2b34eb87df8
DKfork <kingknight1948@gmail.com>
2025-08-14 18:56:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DKfork/DK-MD/blob/main/images/realistic%20burning%20ro.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DK-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
