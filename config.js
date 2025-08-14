9353ac87c5d54e37a67893288e02a4c04e95f75f
DKfork <kingknight1948@gmail.com>
2025-08-14 18:56:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DKfork/DK-MD/blob/main/images/realistic%20burning%20ro.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DK-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94784394420',  // Replace with the owner's phone number
 
 
 
